8b0ab42addec8f4753a293dcc5fe199f9f93f1e9
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2025-04-04 03:38:47 +0530

    Remove unused event listener code for menu button in main.js to improve code clarity

--- main.js
+++ main.js
@@ -43,12 +43,3 @@ document.addEventListener("DOMContentLoaded", () => {
   // Start loading sections in order
   loadSection(0);
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const menuBtn = document.getElementById("menu-btn");
-//   const navLinks = document.getElementById("nav-links");
-
-//   menuBtn.addEventListener("click", function () {
-//     navLinks.classList.toggle("active");
-//   });
-// });
